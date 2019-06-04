ca28adce2635b7680ce19ad684f2ae88c414710c
adrianschung <adrianschung@gmail.com>
2019-06-04 18:38:54 -0400

    Move storage config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,18 +6,19 @@ CarrierWave.configure do |config|
       config.storage = :file
     end
   end
-  
-  # Use AWS storage if in production
-  if Rails.env.production?
-    CarrierWave.configure do |config|
-      config.storage = :fog
-    end
-  end
-  
+
   config.fog_credentials = {
     :provider               => 'AWS',                             # required
     :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],             # required
     :aws_secret_access_key  => ENV["AWS_SECRET_KEY"],             # required
   }
   config.fog_directory  = ENV["AWS_BUCKET"]                       # required
+
+  # Use AWS storage if in production
+
+  if Rails.env.production?
+    CarrierWave.configure do |config|
+      config.storage = :fog
+    end
+  end
 end

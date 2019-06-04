c3c6fb0ccaa22229e2404c523afcdcc90b6278ed
adrianschung <adrianschung@gmail.com>
2019-06-04 18:45:51 -0400

    Add fog provider

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,6 +9,7 @@ CarrierWave.configure do |config|
     end
   end
 
+  config.fog_provider = 'fog/aws'                                 # required
   config.fog_credentials = {
     :provider               => 'AWS',                             # required
     :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],             # required

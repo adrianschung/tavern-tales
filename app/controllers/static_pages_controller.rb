a1d2d837f354d62a9d679195b170eaabd62148fa
adrianschung <adrianschung@gmail.com>
2019-06-06 18:05:04 -0400

    Add recent maps to root page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class StaticPagesController < ApplicationController
-  def index; end
+  def index
+    @recent_maps = Map.order(created_at: :desc).first(2)
+  end
 end

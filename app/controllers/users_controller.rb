fcb5142f75b550867e05575051b770baca5cd3f7
adrianschung <adrianschung@gmail.com>
2019-06-14 22:55:47 -0400

    Change users per page to 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@
 
 class UsersController < ApplicationController
   def index
-    @users = User.paginate(page: params[:page], per_page: 3)
+    @users = User.paginate(page: params[:page], per_page: 10)
   end
 
   def show
